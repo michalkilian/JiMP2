6266776a3fcbe77ba311158ccf9303dbd6827001
misiek10000 <mkilian@mielec.com.pl>
2018-03-05 13:36:24 +0100

    Add double base palindrome directory

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michał on 2018-03-05.
+//
+
+#include "DoubleBasePalindromes.h"
--- lab1/factorial/main.cpp
+++ lab1/factorial/main.cpp
@@ -2,7 +2,7 @@
 #include "Factorial.h"
 
 int main() {
-  int x = -13;
-  std::cout << "factorial of " << x << "  is: " << factorial(x) << std::endl;
-  return 0;
+    int x = -13;
+    std::cout << "factorial of " << x << "  is: " << factorial(x) << std::endl;
+    return 0;
 }
