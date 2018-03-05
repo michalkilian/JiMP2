cee35f0a1c26f34a8b6742320dec18d1106ccb96
misiek10000 <mkilian@mielec.com.pl>
2018-03-05 20:07:23 +0100

    Add working excersise double base palindromes

--- lab1/doublebasepalindromes/DoubleBasePalindromes.h
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -1,8 +1,13 @@
 //
 // Created by Michał on 2018-03-05.
 //
+#include <cstdint>
+#include <iostream>
 
 #ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
 #define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
+uint64_t ReversingNumber(uint64_t number);
 
 #endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
--- lab1/doublebasepalindromes/main.cpp
+++ lab1/doublebasepalindromes/main.cpp
@@ -2,3 +2,11 @@
 // Created by Michał on 2018-03-05.
 //
 
+#include "DoubleBasePalindromes.h"
+#include <iostream>
+
+int main() {
+    int max_vaule_exculsive = 1;
+    std::cin >> max_vaule_exculsive;
+    std::cout << "wynik: " << DoubleBasePalindromes(max_vaule_exculsive);
+}
