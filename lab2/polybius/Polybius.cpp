817cde5795dae86f13513d792e7c8bf39cd05575
misiek10000 <mkilian@mielec.com.pl>
2018-03-12 21:15:01 +0100

    Add Polybius v2 - working

--- lab2/polybius/Polybius.cpp
+++ lab2/polybius/Polybius.cpp
@@ -4,88 +4,93 @@
 
 #include "Polybius.h"
 
+std::map<char, unsigned int> Crypting = {
+        {'A', 11},
+        {'B', 12},
+        {'C', 13},
+        {'D', 14},
+        {'E', 15},
+        {'F', 21},
+        {'G', 22},
+        {'H', 23},
+        {'I', 24},
+        {'J', 24},
+        {'K', 25},
+        {'L', 31},
+        {'M', 32},
+        {'N', 33},
+        {'O', 34},
+        {'P', 35},
+        {'Q', 41},
+        {'R', 42},
+        {'S', 43},
+        {'T', 44},
+        {'U', 45},
+        {'V', 51},
+        {'W', 52},
+        {'X', 53},
+        {'Y', 54},
+        {'Z', 55},
+};
+
+std::map<unsigned int, char> Decrypt = {
+        {11, 'a'},
+        {12, 'b'},
+        {13, 'c'},
+        {14, 'd'},
+        {15, 'e'},
+        {21, 'f'},
+        {22, 'g'},
+        {23, 'h'},
+        {24, 'i'},
+        {24, 'j'},
+        {25, 'k'},
+        {31, 'l'},
+        {32, 'm'},
+        {33, 'n'},
+        {34, 'o'},
+        {35, 'p'},
+        {41, 'q'},
+        {42, 'r'},
+        {43, 's'},
+        {44, 't'},
+        {45, 'u'},
+        {51, 'v'},
+        {52, 'w'},
+        {53, 'x'},
+        {54, 'y'},
+        {55, 'z'},
+};
+
+std::string TextPreparation(std::string message) {
+    message.erase(remove_if(message.begin(), message.end(), [](char c) { return !isalpha(c); }), message.end());
+    std::string readyText;
+    for (int i = 0; i < message.length(); i++) {
+        readyText += toupper(message[i]);
+    }
+    return readyText;
+}
 std::string PolybiusCrypt(std::string message){
-    char tablica[5][5] = {
-            {'A','B','C','D','E'},
-            {'F','G','H','I','K'},
-            {'L','M','N','O','P'},
-            {'Q','R','S','T','U'},
-            {'V','W','X','Y','Z'}
-    };
-    Tabela coordinates;
-    std::string output;
-    for(int i = 0; i < message.length(); ++i){
-        char charToEncrypt = message[i];
-
-        if (charToEncrypt == '\0'){
-            return output;
-        }
-        else{
-            coordinates = Encryption(charToEncrypt, tablica);
-
-            if(coordinates.wiersz == 99){
-                continue;
-            }
-            else{
-                std::cout << coordinates.wiersz;
-                output += coordinates.wiersz;
-                output += coordinates.kolumna;
-            }
-        }
+    std::string preparedText = TextPreparation(message);
+    std::string polybiusEncrypted;
+    int intChar;
+    for (int i = 0; i < preparedText.length(); i++) {
+        intChar = Crypting[preparedText[i]];
+        polybiusEncrypted += std::to_string(intChar);
     }
-
-    return output;
+    return polybiusEncrypted;
 }
+
 std::string PolybiusDecrypt(std::string crypted){
-    int character;
-    int index = 0;
-    char* output;
-    char tablica[5][5] = {
-            {'A','B','C','D','E'},
-            {'F','G','H','I','K'},
-            {'L','M','N','O','P'},
-            {'Q','R','S','T','U'},
-            {'V','W','X','Y','Z'}
-    };
-    for(int i = 0; i<256; ++i){
-
-    }
-//    while(znak != EOF) {
-//        cout << znak << endl; //wypisz na ekranie
-//        char error;
-//        error = Decryption(znak, tablica);
-//
-//        output[index] = error;
-//        index++;
-//        fscanf(input, "%d ", &znak); //pobierz znak
-//    }
-//    return output;
-}
-
-Tabela Encryption(char charToEncrypt, char array[5][5]){
-    Tabela coordinates;
-    for(int i = 0; i < 5; ++i){
-        for(int j = 0; j < 5; ++j){
-            if(charToEncrypt == array[i][j]){
-                coordinates.wiersz = i+1;
-                coordinates.kolumna = j+1;
-                return coordinates;
-            }
+    std::string polybiusDecrypted;
+    for (int i = 0; i < crypted.length(); i = i + 2) {
+        std::string twoInts;
+        if (crypted.length() >= 2) {
+            twoInts += crypted[i];
+            twoInts += crypted[i + 1];
         }
+        int intToCheck = stoi(twoInts, nullptr, 10);
+        polybiusDecrypted += Decrypt[intToCheck];
     }
-    if (charToEncrypt == 'J'){
-        coordinates.kolumna = 4;
-        coordinates.wiersz = 2;
-    } else{
-        coordinates.kolumna = 99;
-        coordinates.wiersz = 99;
-    }
-    return coordinates;
-}
-
-char Decryption(int numberToDecrypt, char array[5][5]){
-    int a, b;
-    b = numberToDecrypt% 10;
-    a = (numberToDecrypt-b)/10;
-    return array[a-1][b-1];
+    return polybiusDecrypted;
 }
--- lab2/polybius/Polybius.h
+++ lab2/polybius/Polybius.h
@@ -5,16 +5,11 @@
 #ifndef JIMP_EXERCISES_POLYBIUS_H
 #define JIMP_EXERCISES_POLYBIUS_H
 
+
 #include <string>
-#include <iostream>
-
-typedef struct {
-    char wiersz, kolumna;
-} Tabela;
-
+#include <map>
+#include <algorithm>
 std::string PolybiusCrypt(std::string message);
 std::string PolybiusDecrypt(std::string crypted);
-Tabela Encryption(char charToEncrypt, char array[5][5]);
-char Decryption(int numberToDecrypt, char array[5][5]);
 
 #endif //JIMP_EXERCISES_POLYBIUS_H
