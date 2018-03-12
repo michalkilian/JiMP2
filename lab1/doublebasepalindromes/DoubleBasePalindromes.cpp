8637a9c923d7020b46319164b537a24ed126c7b0
misiek10000 <mkilian@mielec.com.pl>
2018-03-12 21:07:42 +0100

    Fix Double Base Palindromes

--- lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -5,24 +5,36 @@
 #include "DoubleBasePalindromes.h"
 
 uint64_t DoubleBasePalindromes(int max_vaule_exculsive) {
+    if (max_vaule_exculsive < 1) {
+        return 0;
+    }
+    uint64_t base2[128];
     uint64_t suma = 0;
     uint64_t reversed_number = 0;
-    uint64_t base_2_number = 0;
     for (uint64_t i = 1; i <= max_vaule_exculsive; i += 2) {
         reversed_number = ReversingNumber(i);
-        base_2_number = 0;
-        if (i == reversed_number){
+        if (i == reversed_number) {
             uint64_t checking_number = i;
-            uint64_t position_multiplier = 1;
-            while (checking_number > 0){
-                if (checking_number % 2 == 1){
-                    base_2_number += position_multiplier;
+            uint64_t position_multiplier = 0;
+            while (checking_number > 0) {
+                if (checking_number % 2 == 1) {
+                    base2[position_multiplier] = 1;
+                }
+                else{
+                    base2[position_multiplier] = 0;
                 }
                 checking_number /= 2;
-                position_multiplier *= 10;
+                position_multiplier++;
             }
-            reversed_number = ReversingNumber(base_2_number);
-            if (reversed_number == base_2_number){
+            std::cout << "\n";
+            bool palindrome = true;
+            for (int i = 0; i < position_multiplier; ++i) {
+                if (!(base2[i] == base2[position_multiplier - i - 1])) {
+                    palindrome = false;
+                    break;
+                }
+            }
+            if (palindrome == true) {
                 suma += i;
             }
         }
@@ -30,12 +42,12 @@ uint64_t DoubleBasePalindromes(int max_vaule_exculsive) {
     return suma;
 };
 
-uint64_t ReversingNumber(uint64_t number){
+uint64_t ReversingNumber(uint64_t number) {
     uint64_t checking_number = number;
     uint64_t reversed_number = 0;
 
-    while (checking_number > 0){
-        reversed_number = reversed_number*10 + checking_number%10;
+    while (checking_number > 0) {
+        reversed_number = reversed_number * 10 + checking_number % 10;
         checking_number /= 10;
     }
     return reversed_number;
