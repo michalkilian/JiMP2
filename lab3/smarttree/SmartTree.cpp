76e74fc5a679fb2f2f523abb162573d5342a8e42
misiek10000 <mkilian@mielec.com.pl>
2018-03-19 20:02:58 +0100

    Add smarttree skeleton and travis edit

---
+++ lab3/smarttree/SmartTree.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Michał on 2018-03-19.
+//
+
+#include "SmartTree.h"
+
+namespace datastructures {
+
+    std::unique_ptr<SmartTree> CreateLeaf(int value) {
+
+    }
+
+    std::unique_ptr<SmartTree>
+    InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree) {
+
+    }
+
+    std::unique_ptr<SmartTree>
+    InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree) {
+
+    }
+
+    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out) {
+
+    }
+
+    std::string DumpTree(const std::unique_ptr<SmartTree> &tree) {
+
+    }
+
+    std::unique_ptr<SmartTree> RestoreTree(const std::string &tree) {
+
+    }
+}
